354e0a09caab315ea10c8a16892feac946b210cd
Wojciech Sala <wojtek@wojciechs-mbp.home>
2021-07-15 12:16:27 +0200

    filter context, schema and changeset

---
+++ lib/flat_search/filters/filter.ex
@@ -0,0 +1,24 @@
+defmodule FlatSearch.Filters.Filter do
+  @moduledoc """
+  Filter module, non persistant schema
+  """
+
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @default_params ~w(region city district street max_price)a
+
+  schema "filters" do
+    field :region, :string, virtual: true
+    field :city, :string, virtual: true
+    field :district, :string, virtual: true
+    field :street, :string, virtual: true
+    field :max_price, :integer, virtual: true
+  end
+
+  def changeset(filter, attrs) do
+    filter
+    |> cast(attrs, @default_params)
+    |> validate_inclusion(:max_price, 1..100_000)
+  end
+end
